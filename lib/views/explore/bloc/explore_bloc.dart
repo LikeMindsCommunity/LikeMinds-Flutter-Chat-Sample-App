176c001fd312e4687e1272216ba364edb7265a49
Divyansh Gandhi <divyansh.gandhi@likeminds.community>
2023-01-03 14:06:09 +0530

    Creating explore bloc

--- lib/views/explore/bloc/explore_bloc.dart
+++ lib/views/explore/bloc/explore_bloc.dart
@@ -1,13 +1,48 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:group_chat_example/data/models/explore/space_model.dart';
 
 part 'explore_event.dart';
 part 'explore_state.dart';
 
 class ExploreBloc extends Bloc<ExploreEvent, ExploreState> {
   ExploreBloc() : super(ExploreInitial()) {
-    on<ExploreEvent>((event, emit) {
-      // TODO: implement event handler
+    on<ExploreEvent>((event, emit) async {
+      if (event is InitExploreEvent) {
+        emit(ExploreLoading());
+        await Future.delayed(const Duration(seconds: 2), () {
+          emit(ExploreLoaded(getData()));
+        });
+      } else if (event is RefreshExploreEvent) {
+        emit(ExploreLoading());
+        await Future.delayed(const Duration(seconds: 2), () {
+          emit(ExploreLoaded(getData()));
+        });
+      } else if (event is PinSpaceEvent) {
+        emit(ExploreLoading());
+        await Future.delayed(const Duration(seconds: 2), () {
+          emit(ExploreLoaded(getData()));
+        });
+      }
     });
+  }
+
+  List<SpaceModel> getData() {
+    List<SpaceModel> spaces = [];
+
+    for (int i = 0; i < 10; i++) {
+      spaces.add(SpaceModel(
+        name: 'MM Space $i',
+        description:
+            'Description $i Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imageUrl: 'https://picsum.photos/400/400',
+        id: '$i',
+        isPinned: false,
+        members: (i * 5),
+        messages: i * 5,
+      ));
+    }
+
+    return spaces;
   }
 }
--- lib/views/explore/bloc/explore_state.dart
+++ lib/views/explore/bloc/explore_state.dart
@@ -4,3 +4,11 @@ part of 'explore_bloc.dart';
 abstract class ExploreState {}
 
 class ExploreInitial extends ExploreState {}
+
+class ExploreLoading extends ExploreState {}
+
+class ExploreLoaded extends ExploreState {
+  final List<SpaceModel> spaces;
+
+  ExploreLoaded(this.spaces);
+}
